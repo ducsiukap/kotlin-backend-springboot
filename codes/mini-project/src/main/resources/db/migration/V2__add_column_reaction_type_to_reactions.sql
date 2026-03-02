a053d08edcbffcd40db606bfcd31bc85e50f387f
ducsiukap <DucPV.contact@gmail.com>
2026-03-02 22:59:16 +0700

    update-1

--- codes/mini-project/src/main/resources/db/migration/V2__add_column_reaction_type_to_reactions.sql
+++ codes/mini-project/src/main/resources/db/migration/V2__add_column_reaction_type_to_reactions.sql
@@ -1,5 +1,2 @@
 ALTER TABLE reactions
-    ADD reaction_type VARCHAR(20) NULL;
-
-ALTER TABLE reactions
-    MODIFY reaction_type VARCHAR (20) NOT NULL;
+    ADD reaction_type VARCHAR(20) NOT NULL DEFAULT "LIKE";
